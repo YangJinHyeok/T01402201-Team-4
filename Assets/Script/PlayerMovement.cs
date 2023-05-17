ca0fa0f95938001a7777a50a8a1dfa0be864ca70
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2023-05-17 12:47:48 +0900

    Update PlayerMovement.cs

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    int playerspeed = 10;
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
-    private float dirX = 10f;
-    private float dirY = 10f;
+    private float Playerspeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,22 +31,22 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(new Vector2(dirX * Time.deltaTime, 0));
+            transform.Translate(new Vector2(Playerspeed * Time.deltaTime, 0));
             state = MovementState.right;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(new Vector2(-dirX * Time.deltaTime, 0));
+            transform.Translate(new Vector2(-Playerspeed * Time.deltaTime, 0));
             state = MovementState.left;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(new Vector2(0, dirY  * Time.deltaTime));
+            transform.Translate(new Vector2(0, Playerspeed  * Time.deltaTime));
             state = MovementState.up;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(new Vector2(0, -dirY * Time.deltaTime));
+            transform.Translate(new Vector2(0, -Playerspeed * Time.deltaTime));
             state = MovementState.down;
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
@@ -71,28 +69,5 @@ public class PlayerMovement : MonoBehaviour
         anim.SetInteger("state", (int)state);
     }
     
-/*    private enum MovementState {down, right, up, left};
-    private void UpdateAnimationUpdate()
-    {
-        MovementState state = MovementState.down;
 
-        if(dirX > 10f)
-        {
-            state = MovementState.right;
-        }
-        else if (dirX < 10f)
-        {
-            state = MovementState.left;
-        }
-        else if (dirY > 10f)
-        {
-            state = MovementState.up;
-        }
-        else if (dirY < 10f)
-        {
-            state = MovementState.down;
-        }
-
-        anim.SetInteger("state", (int)state);
-    }*/
 }

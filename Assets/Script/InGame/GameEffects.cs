dd1c64378582933d93ddd8460381db687a96b3a3
seungyeobchoi <98461038+seungyeobchoi@users.noreply.github.com>
2023-06-01 02:57:18 +0900

    fix : add win/lose Image UI func

--- Assets/Script/InGame/GameEffects.cs
+++ Assets/Script/InGame/GameEffects.cs
@@ -112,5 +112,8 @@ public class GameEffects : MonoBehaviour
             }
         }
     }
-    
+    public void endGame(bool isWin)
+    {
+        gameUIController.endSequence(isWin);
+    }
 }

3d2346cd1f221b1a17b5b662d67834e6d3c4e3f0
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2023-06-02 12:41:32 +0900

    bomb
    1234

---
+++ Assets/Script/Bomb.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Bomb : MonoBehaviour
+{
+
+    [Header("Explosion")]
+    public Explosion explosionPrefab;
+    public LayerMask explosionLayerMask;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 3;
+
+    public float time = 3.0f;
+
+
+
+    private void Update()
+    {
+        
+    }
+
+    private IEnumerator ExplodeAfterDelay(GameObject bomb, Vector2 position)
+    {
+        while (time > 0)
+        {
+            Debug.Log(time);
+            yield return null;
+        }
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
+        Destroy(bomb);
+        GameObject.Find("Player").GetComponent<BombController>().bombsRemaining++;
+    }
+
+    public void PlaceBomb()
+    {
+        Vector2 position = GameObject.Find("Player").transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, 0.1f);
+        bool isBombPresent = false;
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.CompareTag("Bomb"))
+            {
+                isBombPresent = true;
+                break;
+            }
+        }
+
+        if (!isBombPresent)
+        {
+            
+            GameObject bombInstance = Instantiate(GameObject.Find("Player").GetComponent<BombController>().bombPrefab, position, Quaternion.identity);
+            Bomb bomb = bombInstance.GetComponent<Bomb>();
+            Debug.Log("Active? " + gameObject.activeInHierarchy);
+            GameObject.Find("Player").GetComponent<BombController>().bombsRemaining--;
+
+            StartCoroutine(ExplodeAfterDelay(bombInstance, position));
+            
+        }
+    }
+
+    private IEnumerator ShowDustAndDestroy(Collider2D collider)
+    {
+        float dustDuration = 1f;
+        Vector3 dustPosition = collider.transform.position;
+
+        Destroy(collider.gameObject);
+        GameObject dustObject = new GameObject("Dust");
+        SpriteRenderer dustRenderer = dustObject.AddComponent<SpriteRenderer>();
+        string spritePath = "dust";
+        Sprite dustSprite = Resources.Load<Sprite>(spritePath);
+        dustRenderer.sprite = dustSprite;
+        dustObject.transform.position = dustPosition;
+
+        yield return new WaitForSeconds(dustDuration);
+
+        Destroy(dustObject);
+    }
+
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            return;
+        }
+        position += direction;
+
+        if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
+        {
+            Collider2D[] colliders = Physics2D.OverlapBoxAll(position, Vector2.one / 2f, 0f, explosionLayerMask);
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.CompareTag("Box"))
+                {
+                    StartCoroutine(ShowDustAndDestroy(collider));
+                }
+                else if (collider.CompareTag("Bomb"))
+                {
+                    
+                }
+            }
+
+            return;
+        }
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+
+
+        Explode(position, direction, length - 1);
+    }
+
+
+}
--- Assets/Script/BombController.cs
+++ Assets/Script/BombController.cs
@@ -7,18 +7,14 @@ using UnityEngine.UIElements;
 
 public class BombController : MonoBehaviour
 {
+
+
     [Header("Bomb")]
     public GameObject bombPrefab;
     public KeyCode inputKey = KeyCode.Space;
     public float bombFuseTime = 3f;
     public int bombAmount = 1;
-    private int bombsRemaining;
-
-    [Header("Explosion")]
-    public Explosion explosionPrefab;
-    public LayerMask explosionLayerMask;
-    public float explosionDuration = 1f;
-    public int explosionRadius = 1;
+    public int bombsRemaining;
 
     public Box boxPrefab;
 
@@ -33,108 +29,10 @@ public class BombController : MonoBehaviour
     {
         if (bombsRemaining > 0 && Input.GetKeyDown(inputKey))
         {
-            StartCoroutine(PlaceBomb());
+            bombPrefab.GetComponent<Bomb>().PlaceBomb();
         }
     }
-    private IEnumerator PlaceBomb()
-    {
-        Vector2 position = transform.position;
-        position.x = Mathf.Round(position.x);
-        position.y = Mathf.Round(position.y);
-
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, 0.1f);
-        bool isBombPresent = false;
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.CompareTag("Bomb"))
-            {
-                isBombPresent = true;
-                break;
-            }
-        }
-
-        if (!isBombPresent)
-        {
-            GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
-            bombsRemaining--;
-
-            yield return new WaitForSeconds(bombFuseTime);
-
-            position = bomb.transform.position;
-            position.x = Mathf.Round(position.x);
-            position.y = Mathf.Round(position.y);
-
-            Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-            explosion.SetActiveRenderer(explosion.start);
-            explosion.DestroyAfter(explosionDuration);
-
-            Explode(position, Vector2.up, explosionRadius);
-            Explode(position, Vector2.down, explosionRadius);
-            Explode(position, Vector2.left, explosionRadius);
-            Explode(position, Vector2.right, explosionRadius);
-
-            Destroy(bomb);
-            bombsRemaining++;
-        }
-    }
-
-    private IEnumerator ShowDustAndDestroy(Collider2D collider)
-    {
-        float dustDuration = 1f;
-        Vector3 dustPosition = collider.transform.position;
-
-        Destroy(collider.gameObject);
-        GameObject dustObject = new GameObject("Dust");
-        SpriteRenderer dustRenderer = dustObject.AddComponent<SpriteRenderer>();
-        string spritePath = "dust";
-        Sprite dustSprite = Resources.Load<Sprite>(spritePath);
-        dustRenderer.sprite = dustSprite;
-        dustObject.transform.position = dustPosition;
-
-        yield return new WaitForSeconds(dustDuration);
-
-        Destroy(dustObject);
-    }
-
-
-    private void Explode(Vector2 position, Vector2 direction, int length)
-    {
-        if (length <= 0)
-        {
-            return;
-        }
-        position += direction;
-
-        if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
-        {
-            Collider2D[] colliders = Physics2D.OverlapBoxAll(position, Vector2.one / 2f, 0f, explosionLayerMask);
-            foreach (Collider2D collider in colliders)
-            {
-                if (collider.CompareTag("Box"))
-                {
-                    StartCoroutine(ShowDustAndDestroy(collider));
-                }
-                else if (collider.CompareTag("Bomb"))
-                {
-                    bombFuseTime = 0f;
-                    Invoke("Explode", bombFuseTime);
-                }
-            }
-
-            return;
-        }
-
-        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
-        explosion.SetDirection(direction);
-        explosion.DestroyAfter(explosionDuration);
-
-        
-        Explode(position, direction, length - 1);
-    }
-
-
-
+     
 
     private void OnTriggerExit2D(Collider2D other)
     {

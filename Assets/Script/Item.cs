e2dd6b2ba265782e9fb8e148d1f96865959026e9
HwangBBang <h1009218@hufs.ac.kr>
2023-06-02 15:15:42 +0900

    Fix Item.cs

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -5,37 +5,20 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    private static Item thisItem;
-
-    private void Awake()
-    {
-        thisItem = this;
-    }
-
-    public static Item GetItem()
-    {
-        return thisItem;
-    }
     
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-    
-
-        // 현재  오브젝트(아이템)과 충돌한 오브젝트의 태그가 Player라면
+    {    
+       // 현재  오브젝트(아이템)과 충돌한 오브젝트의 태그가 Player라면
         if (collision.gameObject.CompareTag("Player"))
         {
-            
-        // 현재 오브젝트(아이템) 리턴
-
+            GameEffects gameEffects = GetComponent<GameEffects>();
+        // 현재 오브젝트(아이템)자체를 넘겨주기
+            gameEffects.powerUp(transform.gameObject);
 
         // 현재 오브젝트(아이템) 삭제
             Destroy(gameObject);
-
-    
-            
         }
-
-
 
     }   
 }
+
